76507bd36e28f989b30dfa2f9ef5a9119eae7e11
dp2483029 <troutkilla1@hotmail.com>
2014-06-26 15:30:16 -0700

    Gaddis

---
+++ Gaddis_7thEd_Chap2_Prob1/main.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.cpp
+ * Author: Petersen, David
+ *
+ * Created on June 26, 2014, 2:52 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    short x=62, y=99, total=0;
+    total = x+y;
+    
+    cout<< total;
+    
+    return 0;
+}
+
